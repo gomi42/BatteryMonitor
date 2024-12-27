7b3dfe60ce3b49759b7d16db98eac0a520cae761
gomi42 <gomi42@gmx.de>
2024-12-27 12:06:10 +0100

    special handling of remaining time

---
+++ WidthConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace BatteryMonitor
+{
+    internal class WidthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var v = (double)value;
+
+            return v - 20;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
