f49a2c0bac1f81bca092d7e048c2d21ad3bec0cc
gomi42 <gomi42@gmx.de>
2024-12-31 12:55:01 +0100

    .

---
+++ AnimatedExpander.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace BatteryMonitor
+{
+    internal class AnimatedExpander : Expander
+    {
+        public AnimatedExpander()
+        {
+            Expanded += ExpanderHasExpanded;
+            Collapsed += ExpanderHasCollapsed;
+        }
+
+        FrameworkElement expandSite;
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            expandSite = GetTemplateChild("ExpandSite") as FrameworkElement;
+        }
+
+        private void ExpanderHasExpanded(object sender, RoutedEventArgs args)
+        {
+        }
+
+        private void ExpanderHasCollapsed(object sender, RoutedEventArgs args)
+        {
+        }
+    }
+
+
+}
