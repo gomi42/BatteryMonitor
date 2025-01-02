4ed576e8db4dcc628c488df89915cee4c838553c
gomi42 <gomi42@gmx.de>
2025-01-02 07:24:08 +0100

    .

---
+++ ViewModelBase.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BatteryMonitor
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        //////////////////////////////////////////////
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //////////////////////////////////////////////
+
+        protected void NotifyPropertyChanged([CallerMemberName] string info = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+
+        //////////////////////////////////////////////
+
+        public void SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(field, value))
+            {
+                return;
+            }
+
+            field = value;
+            NotifyPropertyChanged(propertyName);
+        }
+    }
+}
