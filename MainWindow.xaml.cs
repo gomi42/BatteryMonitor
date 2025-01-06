95143abdcb367c3ebf31ae192a4e759b5f008903
gomi42 <gomi42@gmx.de>
2025-01-06 09:07:49 +0100

    .

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace BatteryMonitor
 {
@@ -23,11 +25,33 @@ namespace BatteryMonitor
         private void ThemeButtonChecked(object sender, RoutedEventArgs e)
         {
             ThemeSettings.SetTheme(this, true);
+            CollapseAllExpanders(this);
         }
 
         private void ThemeButtonUnchecked(object sender, RoutedEventArgs e)
         {
             ThemeSettings.SetTheme(this, false);
+            CollapseAllExpanders(this);
+        }
+
+        public static void CollapseAllExpanders(DependencyObject myVisual)
+        {
+            if (myVisual is ScrollViewer scrollViewer)
+            {
+                myVisual = (DependencyObject)scrollViewer.Content;
+            }
+
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(myVisual); i++)
+            {
+                var childVisual = VisualTreeHelper.GetChild(myVisual, i);
+
+                if (childVisual is AnimatedExpander expander)
+                {
+                    expander.IsExpanded = false;
+                }
+
+                CollapseAllExpanders(childVisual);
+            }
         }
     }
 }
